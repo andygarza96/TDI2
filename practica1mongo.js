f620f359b28f1872cf04e806825c372b1801adc6
andygarza96 <andrea_berrones@hotmail.com>
2018-01-31 22:08:25 -0700

    practica 1

---
+++ practica1mongo.js
@@ -0,0 +1,32 @@
+/*
+1. Descargar el archivo grades.json y en la terminal ejecuta el siguiente comando: $ mongoimport -d students -c grades < grades.json
+
+2. Confirmar que se importo correctamente la colección en la base de datos students con un count en el shell de mongo:
+
+3. Encuentra todas las calificaciones del estudiante con el id número 4
+    -87.89071881934647
+    -27.29006335059361
+    -5.244452510818443
+    -28.656451042441
+    
+4.¿Cuántos registros hay de tipo exam?
+    200
+
+5.¿Cuántos registros hay de tipo homework?
+    400
+    
+6.¿Cuántos registros hay de tipo quiz?
+    200
+    
+7. Elimina todas las calificaciones del estudiante con el id numero 3 y realiza un count a la base de datos 
+    se eliminan 4 calificaciones
+    
+8.¿Qué estudiantes obtuvieron 75.29561445722392 en una tarea?
+    El 9
+
+9. Actualiza las calificaciones del registro con el id 50906d7fa3c412bb040eb591 por 100 
+
+10. ¿A que estudiante le pertenece esa calificación(oid)?
+    Al 6
+
+*/
